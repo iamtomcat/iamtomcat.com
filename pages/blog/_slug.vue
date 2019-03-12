13b5d65355d0d4acb56ecaf11b6f2dfd4492d40b
Tom Clark <tom@fluid-dynamics.ca>
2019-03-13 10:29:35 +1300

    Lat commit before moving to vue press

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,57 +1,24 @@
 <template>
   <section class="section">
-    <h1 v-if="$apollo.loading" class="title is-1 has-text-centered">Loading...</h1>
-    <div v-else>
-        <h1 class="title is-1 has-text-centered">
-        <nuxt-link :to="`/blog/${post.slug}`">
-          {{post.title}}
-        </nuxt-link>
-      </h1>
-      <h2 class="subtitle is-2 has-text-centered">
-        Written By Tom - {{post.date}}
-      </h2>
-      <div class="content is-size-4" v-html="$md.render(post.body)"></div>
-    </div>
+    <h1 class="title is-1 has-text-centered">
+    </h1>
+    <h2 class="subtitle is-2 has-text-centered">
+      Written By Tom -
+    </h2>
+    <div class="content is-size-4" v-html="content"></div>
   </section>
 </template>
 
 <script>
-import gql from 'graphql-tag'
-
 export default {
-  data() {
+  data: () => ({
+    content: ""
+  }),
+  async asyncData ({params}) {
+    const fileContent = await import(`~/static/blog/${params.slug}.md`)
+    console.log("filecontnet:",fileContent)
     return {
-      post: {
-        title: '',
-        body: '',
-        date: '',
-        author: ''
-      }
-    }
-  },
-  apollo: {
-    post: {
-      query: gql`query Post($slug: String!) {
-        post(filter: {
-          slug: {
-            eq: $slug
-          }
-        }) {
-          title
-          body
-          date
-        }
-      }`,
-      prefetch({params}) {
-        return {
-          slug: params.slug
-        }
-      },
-      variables() {
-        return {
-          slug: this.$route.params.slug
-        }
-      }
+      content: fileContent.default
     }
   }
 }
---
+++ static/blog/test.md
@@ -0,0 +1,9 @@
+---
+title: Test
+description: Stuff
+date: 2019-03-01
+---
+
+# Test
+
+This is a stuff to test.
